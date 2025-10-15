8b01e1f66462b6aebfaf6a3fb26812973b1e333e
isayaeli <isayaelib@gmail.com>
2025-10-15 21:19:14 +0300

    Added Kubernetes files for deploymnet

--- src/home/urls.py
+++ src/home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('health/', views.HealthCheckView.as_view(), name='health-check'),
 ]
--- src/home/views.py
+++ src/home/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, HttpResponse # type: ignore
+from django.http import JsonResponse
+from django.views import View
+
 
 # Create your views here.
 def home(request):
     return HttpResponse("Hello, Django with Docker!")
+
+
+class HealthCheckView(View):
+    def get(self, request):
+        return JsonResponse({"status": "healthy", "service": "django"})
