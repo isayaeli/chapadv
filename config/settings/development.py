5d9db2cff9f752985afa288e94394b420c580b2c
isayaeli <isayaelib@gmail.com>
2025-10-17 16:46:32 +0300

    update allowed hosts  in dev settings

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,7 +1,5 @@
 from .base import *
 
+DEBUG = True
 
-DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
-
-# ALLOWED_HOSTS can be comma-separated in env, e.g. "*" or "example.com,10.244.0.58"
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = ['*']
